d720bcd9d515265fbfdf7ff18ccdc0b65cc0f162
Mark Beebe <mbeebe08@gmail.com>
2024-06-29 20:51:42 -0700

    feat: add image carousel and info text to detail screen

--- app/src/main/java/com/mnbpdx/gardenbook/ui/home/HomeScreen.kt
+++ app/src/main/java/com/mnbpdx/gardenbook/ui/home/HomeScreen.kt
@@ -4,6 +4,8 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.content.res.Configuration.UI_MODE_TYPE_NORMAL
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,7 +14,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -25,9 +31,9 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import com.mnbpdx.gardenbook.R
+import com.mnbpdx.gardenbook.model.plantNames
 import com.mnbpdx.gardenbook.ui.Destination
 import com.mnbpdx.gardenbook.ui.GardenBookBottomAppBar
 import com.mnbpdx.gardenbook.ui.GardenBookTopAppBar
@@ -62,33 +68,21 @@ internal fun HomeScreenContent(
             modifier = Modifier
                 .padding(paddingValues)
                 .fillMaxSize(),
-            color = MaterialTheme.colorScheme.primary,
+            color = MaterialTheme.colorScheme.background,
         ) {
-            Column(
+            LazyColumn(
                 modifier = Modifier.fillMaxWidth()
             ) {
-
-                // Temp list
-                val plantNames: List<String> = listOf(
-                    "pothos",
-                    "daisy",
-                    "tomato",
-                    "rosemary",
-                    "greg",
-                )
-
-                plantNames.map { plantName ->
+                items(plantNames) { plantName ->
                     Spacer(modifier = Modifier.height(16.dp))
                     PlantCard(
                         modifier = Modifier
                             .fillMaxWidth()
                             .padding(horizontal = 8.dp),
                         name = plantName,
-                        onPress = onPlantCardPress
+                        onPress = onPlantCardPress,
                     )
                 }
-
-
             }
         }
     }
@@ -103,6 +97,9 @@ private fun PlantCard(
 ) {
     Card(
         modifier = modifier.clickable { onPress(name) },
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 16.dp
+        )
     ) {
         Surface { // TODO: duplicate surface, consider removing
             Row {
